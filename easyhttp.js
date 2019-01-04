d52d546e92e06164c1dbdd08f67989b53587f098
Adam Dipinto <adam.dipinto@gmail.com>
2019-01-04 06:44:51 -0800

    Setup  POST, PUT, and DELETE functions. POST/PUT - taking in url, data, and callback function while DELETE only taking url and callback

--- easyhttp.js
+++ easyhttp.js
@@ -3,12 +3,12 @@ function easyHTTP() {
 }
 
 // Make an HTTP GET Request
-eastHTTP.prototype.get = function(url, callback) {
+easyHTTP.prototype.get = function(url, callback) {
     this.http.open('GET', url, true);
 
     let self = this;
     this.http.onload = function() {
-        if(self.htpp.status === 200) {
+        if(self.http.status === 200) {
             callback(null, self.http.responseText);
         } else {
             callback('Error: ' + self.http.status);
@@ -19,7 +19,47 @@ eastHTTP.prototype.get = function(url, callback) {
 }
 
 // Make an HTTP POST Request
+easyHTTP.prototype.post = function(url, data, callback) {
+    this.http.open('POST', url, true);
+    this.http.setRequestHeader('Content-type', 'application/json');
+
+    let self = this;
+    this.http.onload = function() {
+        callback(null, self.http.responseText);
+        
+    }
+
+    this.http.send(JSON.stringify(data));
+}
 
 // Make an HTTP PUT Request
 
+easyHTTP.prototype.put = function(url, data, callback) {
+    this.http.open('PUT', url, true);
+    this.http.setRequestHeader('Content-type', 'application/json');
+
+    let self = this;
+    this.http.onload = function() {
+        callback(null, self.http.responseText);
+        
+    }
+
+    this.http.send(JSON.stringify(data));
+}
+
 // Make an HTTP DELETE Request
+
+easyHTTP.prototype.delete = function(url, callback) {
+    this.http.open('DELETE', url, true);
+
+    let self = this;
+    this.http.onload = function() {
+        if(self.http.status === 200) {
+            callback(null, 'Post deleted');
+        } else {
+            callback('Error: ' + self.http.status);
+        }
+    }
+
+    this.http.send();
+}
